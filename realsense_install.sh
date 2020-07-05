0647a285f4a8952052c5b065530c69c202f7dcf2
FrankGaoX <37091183+FrankGaoX@users.noreply.github.com>
2020-07-05 15:43:36 -0400

    Update realsense_install.sh

--- realsense_install.sh
+++ realsense_install.sh
@@ -11,7 +11,14 @@ echo Install udev-rules
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
 sudo udevadm control --reload-rules && sudo udevadm trigger 
 mkdir build && cd build
-cmake ../ -DFORCE_LIBUVC=true -DCMAKE_BUILD_TYPE=release -DBUILD_WITH_CUDA=true
+
+# Build with CUDA (default), the CUDA flag is USE_CUDA, ie -DUSE_CUDA=true
+NVCC_PATH=/usr/local/cuda-10.2/bin/nvcc
+export CUDACXX=$NVCC_PATH
+export PATH=${PATH}:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64
+
+cmake ../ -DBUILD_EXAMPLES=true -DFORCE_LIBUVC=true -DBUILD_WITH_CUDA=true -DCMAKE_BUILD_TYPE=release -DBUILD_PYTHON_BINDINGS=bool:true
 make -j2
 sudo make install
 echo -e "\e[92m\n\e[1mLibrealsense script completed.\n\e[0m"
